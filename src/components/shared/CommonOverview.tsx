1cbce3e6f321347592c3db693f09b24a8a110f2c
solaiman-siam <solaimansiam93@gmai.com>
2025-07-30 11:52:27 +0600

    get in touch section modified

--- src/components/shared/CommonOverview.tsx
+++ src/components/shared/CommonOverview.tsx
@@ -6,12 +6,12 @@ import { localImages } from "@/utils/imageProvider";
 
 const CommonOverview = ({titleText} : {titleText: string}) => {
   return (
-    <div className="pb-24">
+    <div className="pb-24 flex justify-center items-center">
       <Container>
-        <div className="rounded-xl relative flex justify-center items-center py-10 flex-col gap-2 bg-primaryBlack ">
-          <h3 className="text-3xl font-medium pb-2 text-white/90">{titleText}</h3>
-          <div className="flex items-center pt-4 translate-x-6">
-            <button className="px-5 flex cursor-pointer items-center gap-3 group py-3 rounded-lg bg-white text-primaryBlack">
+        <div className="rounded-xl w-1/2 mx-auto  relative flex justify-center items-center py-5 flex-col gap-2 bg-primaryBlack ">
+          <h3 className="text-xl font-medium pb-2 text-white/90">{titleText}</h3>
+          <div className="flex items-center pt-4 ">
+            <button className="px-5 flex cursor-pointer text-[15px] items-center gap-3 group py-2 rounded-lg bg-white text-primaryBlack">
               Get In Touch
               <span>
                 <MoveRight
@@ -21,7 +21,7 @@ const CommonOverview = ({titleText} : {titleText: string}) => {
               </span>
             </button>
           </div>
-          <Image className="absolute w-96 top-0 right-0" src={localImages.StatisticsVector} alt="statistics-vector"/>
+          <Image className="absolute w-52 top-0 right-0" src={localImages.StatisticsVector} alt="statistics-vector"/>
         </div>
       </Container>
     </div>
